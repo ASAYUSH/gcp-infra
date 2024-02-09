7bc25f593631efd8773c69cda962712112d29b66
ayush <uberhack75@gmail.com>
2024-02-09 23:03:19 +0530

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.15.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "famous-segment-405421"
+  zone = "asia-south1-c"
+  credentials = "./key.json"
+}
